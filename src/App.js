788985958ba631244efa22218875199e6c21727d
Huseyn <huseyn.mikayil95@gmail.com>
2020-06-04 09:59:37 +0400

    redux implemented

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
 import TechListModal from "./components/techs/TechListModal";
+import  {Provider} from  'react-redux';
+import store from './store';
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -18,6 +20,7 @@ const App = () => {
   });
 
   return (
+    <Provider store ={store}>
     <Fragment>
       <SearchBar />
       <div className="container">
@@ -29,6 +32,7 @@ const App = () => {
         <Logs />
       </div>
     </Fragment>
+    </Provider>
   );
 };
 
